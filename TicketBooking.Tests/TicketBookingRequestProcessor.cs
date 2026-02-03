e145799603a8c748daad7f1a032b3eecefe798a1
Mesihovic <mersiha.mesihovic.webd25jon@edu.tucsweden.se>
2026-02-03 16:53:11 +0100

    Start Book method in request processor

--- TicketBooking.Tests/TicketBookingRequestProcessor.cs
+++ TicketBooking.Tests/TicketBookingRequestProcessor.cs
@@ -2,5 +2,9 @@ namespace TicketBookingCore.Tests
 {
     public class TicketBookingRequestProcessor
     {
+        internal TicketBookingResponse Book(TicketBookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
