e3af85cdc14a5d5f4a16086f46b0874c5362632f
Mesihovic <mersiha.mesihovic.webd25jon@edu.tucsweden.se>
2026-02-03 16:59:39 +0100

    Add AAA test with asserts

--- TicketBooking.Tests/TicketBookingRequestProcessorTests.cs
+++ TicketBooking.Tests/TicketBookingRequestProcessorTests.cs
@@ -1,17 +1,29 @@
+using Xunit;
+
 namespace TicketBookingCore.Tests
 {
     public class TicketBookingRequestProcessorTests
     {
+        [Fact]
         public void ShouldReturnTicketBookingResultWithRequestValues()
         {
+            // Arrange
             var processor = new TicketBookingRequestProcessor();
-
             var request = new TicketBookingRequest
             {
                 FirstName = "Mersiha",
                 LastName = "Mesihovic",
                 Email = "mersiha.mesihovic@gmail.com"
             };
+
+            // Act
+            TicketBookingResponse response = processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
         }
     }
 }
