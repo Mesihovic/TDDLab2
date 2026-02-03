177fb716d554351746594f56eb26992b2e4bbec0
Mesihovic <mersiha.mesihovic.webd25jon@edu.tucsweden.se>
2026-02-03 16:43:31 +0100

    Add TicketBookingRequest class

---
+++ TicketBooking.Tests/TicketBookingRequest.cs
@@ -0,0 +1,9 @@
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
